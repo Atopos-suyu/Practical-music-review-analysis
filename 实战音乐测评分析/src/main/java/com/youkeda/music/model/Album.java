aa4531edd7cf8882d71f4d6d4f94e1912ef99c8a
Atopos <143861703+Atopos-suyu@users.noreply.github.com>
2023-10-09 09:11:02 +0800

    Update Album.java
    添加注释

--- 实战音乐测评分析/src/main/java/com/youkeda/music/model/Album.java
+++ 实战音乐测评分析/src/main/java/com/youkeda/music/model/Album.java
@@ -3,12 +3,12 @@ package com.youkeda.music.model;
 /**
  *
  */
-public class Album {
+public class Album {  //表示一个音乐专辑对象
 
-  private String id;
-  private String name;
-  private String picUrl;
-
+  private String id;  //音乐专辑的唯一标识符
+  private String name;  //音乐专辑的名称
+  private String picUrl;  //音乐专辑的封面URL
+  //封装，对Album对象的属性进行设置和获取
   public String getId() {
     return id;
   }
