ee8d0be1e0b2e69c2699ebd75678665d241d762e
Atopos <143861703+Atopos-suyu@users.noreply.github.com>
2023-10-09 09:25:43 +0800

    Update Comment.java
    添加注释

--- 实战音乐测评分析/src/main/java/com/youkeda/music/model/Comment.java
+++ 实战音乐测评分析/src/main/java/com/youkeda/music/model/Comment.java
@@ -7,9 +7,9 @@ public class Comment {
 
   private String id;
   private String content;
-  private String likedCount;
-  private String time;
-  private User commentUser;
+  private String likedCount;  //表示评论被点赞的次数
+  private String time;  //表示评论的发表时间
+  private User commentUser;  //发表该评论的用户对象
 
   public String getId() {
     return id;
