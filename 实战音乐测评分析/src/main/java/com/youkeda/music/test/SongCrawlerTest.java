bf54eac3059b23ae1ad2b72f2b5807e1e2744049
Atopos <143861703+Atopos-suyu@users.noreply.github.com>
2023-10-10 10:31:21 +0800

    Update SongCrawlerTest.java
    添加注释

--- 实战音乐测评分析/src/main/java/com/youkeda/music/test/SongCrawlerTest.java
+++ 实战音乐测评分析/src/main/java/com/youkeda/music/test/SongCrawlerTest.java
@@ -8,25 +8,25 @@ import com.youkeda.music.service.impl.SongCrawlerServiceImpl;
 /**
  * 检查服务是否可以正确返回对象
  */
-public class SongCrawlerTest {
+public class SongCrawlerTest {  //歌曲爬虫的测试类
 
   private static final String SA_DING_DING = "萨顶顶";
   private static final String A_ID = "9270";
   private static final String ZUO_SHOU_ZHI_YUE = "左手指月";
   private static final String S_ID = "536096151";
 
-  public static void main(String[] args) {
-    SongCrawlerService songService = new SongCrawlerServiceImpl();
-    songService.start(A_ID);
+  public static void main(String[] args) {  //测试歌曲爬虫能否正确抓取指定歌手的歌曲信息
+    SongCrawlerService songService = new SongCrawlerServiceImpl();  //创建实例，将实现类SongCrawlerServiceImpl对象赋值给它
+    songService.start(A_ID);  //传入ID号启动歌曲爬虫，开始抓取对应歌手的相关信息
 
-    Artist artist = songService.getArtist(A_ID);
+    Artist artist = songService.getArtist(A_ID);  //获取对应歌手的详细信息，并将结果赋值给变量artist
     System.out.println("歌单名称：" + artist.getName());
-    if (!SA_DING_DING.equals(artist.getName())) {
-      System.out.println("歌单名称错误，不是本测试用例指定的歌单。");
-      System.exit(1);
+    if (!SA_DING_DING.equals(artist.getName())) {  //判断是否与常量SA_DING_DING中指定的歌单名称相同
+      System.out.println("歌单名称错误，不是本测试用例指定的歌单。");  //不同则输出错误提示信息
+      System.exit(1);  //调用方法结束程序执行
     }
 
-    Song song = songService.getSong(A_ID, S_ID);
+    Song song = songService.getSong(A_ID, S_ID);  //获取特定歌手的指定歌曲信息
     System.out.println("歌曲名称：" + song.getName());
     if (!ZUO_SHOU_ZHI_YUE.equals(song.getName())) {
       System.out.println("歌曲名称错误，不是本测试用例指定的歌曲。");
